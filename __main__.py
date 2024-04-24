a8ff85c9e4537e2c7d13ec6113a076b37439421b
MikeStork <bocimaister@gmail.com>
2024-04-24 17:24:38 +0200

    TODO in main file

--- __main__.py
+++ __main__.py
@@ -1,7 +1,16 @@
+import arqpack.binarySymmetricChannel
+import arqpack.checksums
 
+# Todo
+#   get data from file (str representing chain of bits)
+#   send data to file (str representing chain of bits)
+#   calculate BER™ and DataRedundancy™
+#   automate tests
+#   automate plot generation
+#   automate raport generation
 
-"""_summary_
-This file is used to perform and connect various actions from arqpack
-"""
 def __main__():
+    """
+    This file is used to perform and connect various actions from arqpack
+    """
     return
